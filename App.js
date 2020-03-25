31295c6bbfa4f0f4faa6df4efc036f9b4c759f22
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2020-03-25 03:24:16 +0300

    code identation fixed

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import { store, persistor } from './store';
+import PropTypes from 'prop-types';
+import { store, persistor } from './src/store';
 
-import AppNavigator from './navigation/AppNavigator';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
@@ -21,43 +21,39 @@ export default function App(props) {
         onFinish={() => handleFinishLoading(setLoadingComplete)}
       />
     );
-  } else {
-    return (
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-        </PersistGate>
-      </Provider>
-    );
   }
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      </PersistGate>
+    </Provider>
+  );
 }
 
 async function loadResourcesAsync() {
-
   await Promise.all([
- 
     Font.loadAsync({
-      // This is the font that we are using for our tab bar
       ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+      'brown-regular': require('./src/assets/fonts/Brown-Regular.ttf'),
     }),
   ]);
 }
 
 function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
   console.warn(error);
 }
 
 function handleFinishLoading(setLoadingComplete) {
   setLoadingComplete(true);
 }
+
+App.propTypes = {
+  skipLoadingScreen: PropTypes.func,
+};
 
 const styles = StyleSheet.create({
   container: {
