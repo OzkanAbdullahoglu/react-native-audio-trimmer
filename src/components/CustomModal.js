acddf03677ccd4eb2c69ef4541184f31b53c8a63
Özkan Abdullahoğlu <ozkanabdullahoglu@Ozkan-Mac-mini.local>
2020-03-29 17:52:52 +0300

    code identation is fixed

--- src/components/CustomModal.js
+++ src/components/CustomModal.js
@@ -1,4 +1,3 @@
-/* eslint linebreak-style: ["error", "windows"]*/
 import React from 'react';
 import {
   Modal,
--- src/components/PlayerContainer.js
+++ src/components/PlayerContainer.js
@@ -21,7 +21,7 @@ class PlayerContainer extends React.Component {
       soundDuration: null,
       shouldPlay: false,
       volume: 1.0,
-      isLoading: true,
+      isLoading: true
     };
   }
 
@@ -34,11 +34,10 @@ class PlayerContainer extends React.Component {
       shouldDuckAndroid: true,
       interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
       playThroughEarpieceAndroid: false,
-      staysActiveInBackground: false,
+      staysActiveInBackground: false
     });
     await this.setSound();
   }
-
 
   shouldComponentUpdate(nextProps, nextState) {
     const {
@@ -48,11 +47,9 @@ class PlayerContainer extends React.Component {
       soundDuration,
       shouldPlay,
       volume,
-      isLoading,
+      isLoading
     } = this.state;
-    const {
-      fileUri,
-    } = this.props;
+    const { fileUri } = this.props;
     return (
       isLoading !== nextState.isLoading ||
       isPLaying !== nextState.isPLaying ||
@@ -65,74 +62,82 @@ class PlayerContainer extends React.Component {
     );
   }
 
-  componentWillUnmount() {
-    this.unloadSound();
+  componentDidUpdate(prevProps, prevState) {
+    const { soundPosition, soundDuration } = this.state;
+
+    if (soundPosition === soundDuration && this.sound !== null) {
+      this.sound.stopAsync();
+    }
   }
-    onPlayPausePressed = async () => {
-      if (this.sound != null) {
-        if (this.state.isPlaying) {
-          this.sound.pauseAsync();
-        } else {
-          this.sound.playAsync();
-        }
-      }
-    };
 
-    onSeekSliderValueChange = () => {
-      if (this.sound != null && !this.isSeeking) {
-        this.isSeeking = true;
-        this.shouldPlayAtEndOfSeek = this.state.shouldPlay;
+  async componentWillUnmount() {
+    await this.unloadSound();
+  }
+
+  onPlayPausePressed = async () => {
+    if (this.sound != null) {
+      if (this.state.isPlaying) {
         this.sound.pauseAsync();
-      }
-    };
-
-    onSeekSliderSlidingComplete = async (value) => {
-      if (this.sound != null) {
-        this.isSeeking = false;
-        const seekPosition = value * this.state.soundDuration;
-        if (this.shouldPlayAtEndOfSeek) {
-          this.sound.playFromPositionAsync(seekPosition);
-        } else {
-          this.sound.setPositionAsync(seekPosition);
-        }
-      }
-    };
-
-    getSeekSliderPosition() {
-      if (
-        this.sound != null &&
-            this.state.soundPosition != null &&
-            this.state.soundDuration != null
-      ) {
-        return this.state.soundPosition / this.state.soundDuration;
-      }
-      return 0;
-    }
-
-    setSound = async () => {
-      if (this.sound !== null) {
-        await this.sound.unloadAsync();
-        this.sound.setOnPlaybackStatusUpdate(null);
-        this.sound = null;
-        /* this.sound = null;*/
-      }
-      try {
-        const { sound } = await Audio.Sound.createAsync(
-          { uri: this.props.fileUri },
-          this.updateScreenForSoundStatus,
-          null,
-          true);
-        this.sound = sound;
-        this.sound.setOnPlaybackStatusUpdate(
-          this.updateScreenForSoundStatus,
-        );
-        this.setState({
-          isLoading: false,
-        });
-      } catch (error) {
-        console.warn(error);
+      } else {
+        this.sound.playAsync();
       }
     }
+  };
+
+  onSeekSliderValueChange = () => {
+    if (this.sound != null && !this.isSeeking) {
+      this.isSeeking = true;
+      this.shouldPlayAtEndOfSeek = this.state.shouldPlay;
+      this.sound.pauseAsync();
+    }
+  };
+
+  onSeekSliderSlidingComplete = async value => {
+    if (this.sound != null) {
+      this.isSeeking = false;
+      const seekPosition = value * this.state.soundDuration;
+      if (this.shouldPlayAtEndOfSeek) {
+        this.sound.playFromPositionAsync(seekPosition);
+      } else {
+        this.sound.setPositionAsync(seekPosition);
+      }
+    }
+  };
+
+  getSeekSliderPosition() {
+    if (
+      this.sound != null &&
+      this.state.soundPosition != null &&
+      this.state.soundDuration != null
+    ) {
+      return this.state.soundPosition / this.state.soundDuration;
+    }
+    return 0;
+  }
+
+  setSound = async () => {
+    if (this.sound !== null) {
+      await this.sound.unloadAsync();
+      this.sound.setOnPlaybackStatusUpdate(null);
+      this.sound = null;
+      /* this.sound = null;*/
+    }
+    try {
+      const { sound } = await Audio.Sound.createAsync(
+        { uri: this.props.fileUri },
+        this.updateScreenForSoundStatus,
+        null,
+        true
+      );
+      this.sound = sound;
+      this.sound.setOnPlaybackStatusUpdate(this.updateScreenForSoundStatus);
+      this.setState({
+        isLoading: false
+      });
+    } catch (error) {
+      console.warn(error);
+    }
+  };
 
   unloadSound = async () => {
     if (this.sound !== null) {
@@ -142,70 +147,70 @@ class PlayerContainer extends React.Component {
       /* this.sound = null;*/
     }
     await this.sound.unloadAsync();
-  }
+  };
 
-    updateScreenForSoundStatus = (status) => {
-      if (status.isLoaded) {
-        this.setState({
-          soundDuration: status.durationMillis,
-          soundPosition: status.positionMillis,
-          shouldPlay: status.shouldPlay,
-          isPlaying: status.isPlaying,
-          volume: 0.9,
-          isPlaybackAllowed: true,
-          isLooping: true,
-        });
-      } else {
-        this.setState({
-          soundDuration: null,
-          soundPosition: null,
-          isPlaybackAllowed: false,
-          isLooping: false,
-        });
-        if (status.error) {
-          console.log(`FATAL PLAYER ERROR: ${status.error}`);
-        }
+  updateScreenForSoundStatus = status => {
+    if (status.isLoaded) {
+      this.setState({
+        soundDuration: status.durationMillis,
+        soundPosition: status.positionMillis,
+        shouldPlay: status.shouldPlay,
+        isPlaying: status.isPlaying,
+        volume: 0.9,
+        isPlaybackAllowed: true,
+        isLooping: true
+      });
+    } else {
+      this.setState({
+        soundDuration: null,
+        soundPosition: null,
+        isPlaybackAllowed: false,
+        isLooping: false
+      });
+      if (status.error) {
+        console.log(`FATAL PLAYER ERROR: ${status.error}`);
       }
-    };
-
-    render() {
-      return (
-        <View style={styles.container}>
-          <NavigationEvents onDidBlur={this.unloadSound} />
-          {this.state.isPlaying ? (
-            <Icon
-              raised
-              name="pause"
-              type="font-awesome"
-              color="#e47a89"
-              disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
-              onPress={this.onPlayPausePressed}
-            />
-          ) : (
-            <Icon
-              raised
-              name="play"
-              type="font-awesome"
-              color="#e47a89"
-              disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
-              onPress={this.onPlayPausePressed}
-            />
-          )}
-          <Slider
-            style={styles.playbackSlider}
-            value={this.getSeekSliderPosition()}
-            minimumValue={0}
-            maximumValue={1}
-            onValueChange={this.onSeekSliderValueChange}
-            onSlidingComplete={this.onSeekSliderSlidingComplete}
-            minimumTrackTintColor="#ecf7ff"
-            maximumTrackTintColor="#2089dc"
-            thumbTintColor="#e47a89"
-            disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
-          />
-        </View>
-      );
     }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <NavigationEvents onDidBlur={this.unloadSound} />
+        {this.state.isPlaying ? (
+          <Icon
+            raised
+            name="pause"
+            type="font-awesome"
+            color="#e47a89"
+            disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
+            onPress={this.onPlayPausePressed}
+          />
+        ) : (
+          <Icon
+            raised
+            name="play"
+            type="font-awesome"
+            color="#e47a89"
+            disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
+            onPress={this.onPlayPausePressed}
+          />
+        )}
+        <Slider
+          style={styles.playbackSlider}
+          value={this.getSeekSliderPosition()}
+          minimumValue={0}
+          maximumValue={1}
+          onValueChange={this.onSeekSliderValueChange}
+          onSlidingComplete={this.onSeekSliderSlidingComplete}
+          minimumTrackTintColor="#ecf7ff"
+          maximumTrackTintColor="#2089dc"
+          thumbTintColor="#e47a89"
+          disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
+        />
+      </View>
+    );
+  }
 }
 
 PlayerContainer.propTypes = {
